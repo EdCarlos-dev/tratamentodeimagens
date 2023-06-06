91963f2e881569ece45ab76216fee69777148161
Edcarlos-dev <bio_edc@hotmail.com>
2023-06-06 11:26:08 -0300

    crete a front to the project

---
+++ front/show_image.py
@@ -0,0 +1,11 @@
+import os   
+import cv2     
+import pandas as pd   
+import matplotlib.pyplot as plt   
+import numpy as np
+import matplotlib.image as image 
+
+placa = cv2.imread('data/input/placa_carro1.png')
+cv2.imshow('a',placa)
+
+# trazer para este script
